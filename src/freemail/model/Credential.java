d7da8fd1017b35ed472433fb6b2f17a13edd4a48
Andrés Bravo <andresbravomontes89@gmail.com>
2019-04-23 20:53:47 +0200

    addAccount

--- src/freemail/model/Credential.java
+++ src/freemail/model/Credential.java
@@ -7,11 +7,13 @@ public class Credential {
 	
 	private InternetAddress mailAddress;
 	private String password;
+	private String name;
 	
-	public Credential(String mailAdress, String password) {
+	public Credential(String mailAdress, String password, String name) {
 		try {
 			this.mailAddress = new InternetAddress(mailAdress);
 			this.password = password;
+			this.name = name;
 		} catch (AddressException e) {
 			e.printStackTrace();
 		}
@@ -24,4 +26,13 @@ public class Credential {
 	public String getPassword() {
 		return password;
 	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
